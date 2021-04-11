a09e16adf9184241454e6e6b84176ee0f63dc2ad
Elena-Weber <crochetmiracles@gmail.com>
2021-04-11 16:34:03 -0400

    commit tree f58eae89b4dbf16366ad3813230c66d640845dc5

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,55 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index' 
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do 
+    @figure = Figure.create(params[:figure])
+    if !params["landmark"]["name"].empty? || !params["title"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    redirect "figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do 
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'/figures/edit'
+  end
+
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params["figure"])
+
+    # if !params[:figure].keys.include?("landmark_ids")
+    #   params[:figure]["landmark_ids"] = []
+    # end
+    # if !params[:figure].keys.include?("title_ids")
+    #   params[:figure]["title_ids"] = []
+    # end
+    
+    if !params["landmark"]["name"].empty? || !params["title"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    #@figure.save
+    redirect "figures/#{@figure.id}"
+  end
+
 end
